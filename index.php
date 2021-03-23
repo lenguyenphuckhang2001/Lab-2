7f7786df809c2bd7740b872f4c50656659f5acec
lenguyenphuckhang2001 <80748946+lenguyenphuckhang2001@users.noreply.github.com>
2021-03-23 17:03:44 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.0.0-beta2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300&display=swap" rel="stylesheet">
+    <title>Web Page</title>
+</head>
+
+<body>
+    <div class="menu">
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <ul class="sub-menu">
+                    <li><a href="#">Product</a></li>
+                    <li><a href="#">All product</a></li>
+                    <li><a href="#">Coat</a></li>
+                    <li><a href="#">Hoodie</a></li>
+                </ul>
+                <li><a href="https://shopee.vn/ontop.brand">Shoppee</a></li>
+                <li><a href="https://www.instagram.com/ontop.brand/">Instagram</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+    <section>
+        <article>
+            <h1>Inspection</h1>
+            <p>Mirum est notare quam littera gothia,<br>quam nunc putamus parum claram.</p>
+            <div class="price"> $300.00</div>
+            <ul>
+                <li>Auto Product Liability</li>
+                <li>Crib Recall</li>
+                <li>Defective Medical Device</li>
+                <li>Food Poisoning</li>
+            </ul>
+            <button>Contact me</button>
+        </article>
+        <article>
+            <h1>Diagnostic</h1>
+            <p>Mirum est notare quam littera gothia,<br>quam nunc putamus parum claram.</p>
+            <div class="price"> $400.00</div>
+            <ul>
+                <li>Auto Product Liability</li>
+                <li>Crib Recall</li>
+                <li>Defective Medical Device</li>
+                <li>Food Poisoning</li>
+            </ul>
+            <button>Contact me</button>
+        </article>
+        <article>
+            <h1>Upgrades</h1>
+            <p>Mirum est notare quam littera gothia,<br>quam nunc putamus parum claram.</p>
+            <div class="price"> $500.00</div>
+            <ul>
+                <li>Auto Product Liability</li>
+                <li>Crib Recall</li>
+                <li>Defective Medical Device</li>
+                <li>Food Poisoning</li>
+            </ul>
+            <button>Contact me</button>
+        </article>
+    </section>
+</body>
+
+</html>
